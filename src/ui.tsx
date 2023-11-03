bd56354868bea983c435b1f21062c205285bcf94
pbroom <116581966+pbroom@users.noreply.github.com>
2023-11-02 20:39:47 -0400

    still cleaning

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,11 +1,4 @@
 import '!./dist/tailwind.css';
-import { ColorArea } from './components/colorArea';
-import { ColorSlider } from './components/colorSlider';
-import { ColorPicker } from './components/colorPicker';
-import { ColorField } from './components/colorField';
-import { Color, parseColor } from '@react-stately/color';
-import { hexToHSB } from './utility';
-import ThemeColor from './theme-color';
 import {
 	defaultColor,
 	defaultName,
